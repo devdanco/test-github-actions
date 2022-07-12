a03d4ac7e4c6a6f8670f9e5ede8e5955ff360c30
Michal <devdanco@gmail.com>
2022-07-12 09:33:17 +0200

    fix: working

--- release.config.js
+++ release.config.js
@@ -4,13 +4,8 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      ("@semantic-release/changelog",
-      {
-        changelogFile: "CHANGELOG.md"
-      })
-    ],
     "@semantic-release/npm",
+    "@semantic-release/changelog",
     [
       ("@semantic-release/git",
       {
