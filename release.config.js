7ba571a2bb37c84024a455b7b0e582319fb5226d
Michal <devdanco@gmail.com>
2022-07-12 09:11:18 +0200

    fix: super

--- release.config.js
+++ release.config.js
@@ -10,12 +10,14 @@ module.exports = {
         changelogFile: "CHANGELOG.md"
       }
     ],
-    "@semantic-release/github",
     [
       "@semantic-release/git",
       {
-        assets: ["CHANGELOG.md", "package.json"]
+        assets: ["CHANGELOG.md", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }
-    ]
+    ],
+    "@semantic-release/github"
   ]
 };
